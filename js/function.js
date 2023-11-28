ecc7d20279e6a41072189f5d67dfd3fbc3b3d377
YumiN <yuminishi0309@gmail.com>
2023-11-28 19:09:48 +0900

    add: delete-btn-function

--- js/function.js
+++ js/function.js
@@ -2,39 +2,36 @@ const inputText = document.querySelector('#input-text');
 const inputButton = document.querySelector('#input-button');
 const list = document.querySelector('#list');
 
-// addTask関数 3つの役割
-// 1. 入力したタスクの追加
-// 2. タスクに削除ボタンを付与
-// 3. 削除ボタンにクリックイベントを付与
-  const addTasks = (task) => {
-  //↑引数taskを受け取る以下の内容の関数を作って、新規作成した変数addTasksに入れろ
-    // 1.入力したタスクの追加----------------
-      // ↓input空だったら処理を止める
-    const value = inputText.value
-    if (!value) return
-    const listItem = document.createElement('li');
-      // li要素を作って、新規作成したlistItemに入れろ
-    const showItem = list.appendChild(listItem);
-      // #listのulの子要素にlistItem(作ったli)を追加して、新規作成した変数showItemに入れろ
-    showItem.innerHTML = task;
-      // 入力したタスクを表示する
+const addTasks = (task) => {
+  // 1.入力したタスクの追加----------------
+  if (!task) return
+  const listItem = document.createElement('li');
+  const showItem = list.appendChild(listItem);
+  showItem.innerHTML = task;
 
-    // 2. タスクに削除ボタンを付与----------------
-    const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = "Delete";
-    listItem.appendChild(deleteButton);
+  // 2. タスクに削除ボタンを付与----------------
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = "Delete";
+  listItem.appendChild(deleteButton);
 
-    // 3. 削除ボタンにクリックイベントを付与----------------
-    deleteButton.addEventListener('click', (evt) => {
-      evt.preventDefault();
-
-      
-    });
+  // 3. 削除ボタンをクリックし、イベントを発動（タスク削除）----------------
+  deleteButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    deleteTasks(deleteButton);
+  });
 };
 
+// 追加ボタンをクリックし、イベントを発動（タスク追加）
 inputButton.addEventListener('click', (evt) =>{
   evt.preventDefault();
   const task = inputText.value;
   addTasks(task);
   inputText.value = '';
 });
+
+// 削除ボタンにタスクを消す機能を追加
+const deleteTasks = (deleteButton) => {
+  const chosenTask = deleteButton.closest('li')
+  chosenTask.remove();
+};
+
